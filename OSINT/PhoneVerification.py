ea4d08851263fee5227ed80f6601365392e742a6
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2023-08-03 14:21:57 +0200

    Create PhoneVerification.py

---
+++ OSINT/PhoneVerification.py
@@ -0,0 +1,19 @@
+#import shlex
+import subprocess
+#import json
+
+#Needed : An API Key from apilayer 
+#Subscribe to Number Verification API :
+#https://apilayer.com/?e=Sign+Up&l=Success
+#https://apilayer.com/marketplace/number_verification-api#documentation-tab
+# Sample of an API key (XXX at the end to mask the real one ;) ) : zUxrKHe83a3wLd7yIVECJEZ4XXXXXXXX
+
+CurlUrl = "curl --request GET 'https://api.apilayer.com/number_verification/validate?number=14158586273' \
+--header 'apikey: zUxrKHe83a3wLd7yIVECJEZ4XXXXXXXX'"
+
+status, output = subprocess.getstatusoutput(CurlUrl)
+
+print(status)
+print(output)
+
+
