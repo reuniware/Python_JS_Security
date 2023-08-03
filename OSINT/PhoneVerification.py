aa5d2e50b4fc00da6807a61531ae2d8eb7add30f
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2023-08-03 14:22:43 +0200

    Update PhoneVerification.py

--- OSINT/PhoneVerification.py
+++ OSINT/PhoneVerification.py
@@ -7,6 +7,7 @@ import subprocess
 #https://apilayer.com/?e=Sign+Up&l=Success
 #https://apilayer.com/marketplace/number_verification-api#documentation-tab
 # Sample of an API key (XXX at the end to mask the real one ;) ) : zUxrKHe83a3wLd7yIVECJEZ4XXXXXXXX
+# 1000 requests/month in the free version
 
 CurlUrl = "curl --request GET 'https://api.apilayer.com/number_verification/validate?number=14158586273' \
 --header 'apikey: zUxrKHe83a3wLd7yIVECJEZ4XXXXXXXX'"
